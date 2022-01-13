7a089fcf130a69d59106210d9a6ade2b5cd79e8c
baileyalo <90293555+baileyalo@users.noreply.github.com>
2022-01-13 02:33:16 -0500

    update

--- nft-frontend/src/App.js
+++ nft-frontend/src/App.js
@@ -1,14 +1,44 @@
-import './App.css';
-
-function App() {
-
-
-  
-  return (
-    <div className="App">  
-   
-    </div>
-  );
-}
-
+import React, {Component} from 'react';
+class App extends Component {
+      state = {
+          isLoading: true,
+          NFT: [],
+          error: null
+      };
+      getNft() {
+          this.setState({loading: true
+          }, () => {
+              fetch("http://localhost:3000/api/nft").then(res => res.json()).then(result => this.setState({
+                  loading: false,nft: result
+              })).catch(console.log);
+          });
+      }
+      componentDidMount() {this.getNft();
+      }
+      render() {
+          const {NFT,error} = this.state;
+          return (
+            <React.Fragment>
+            <h1>NFT</h1>
+            {
+                  error ? <p>
+            {
+                      error.message
+                  } </p> : null}  {
+                      NFT.map((nft1, n) => {
+                          const {
+                             attributes,value,description,image
+                          } = nft1;
+                          return (
+                          <div key={n}><p>Trait Type: {attributes}</p><p>Value: {value}</p>
+                              <p>Description: {description}</p><p>Image Tag: {image}</p>
+                              <hr />
+                          </div>
+                          );
+                      })
+                     } </React.Fragment>);
+          
+          }
+      }
+    
 export default App;
